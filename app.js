49a578847a8187306cb071bc6fee41762b97768f
Gavin <Gavinszczesniak@gmail.com>
2022-02-10 21:17:18 -0500

    Adding sidebar to mobile version, not complete. Still looking into why the side bar button isn't working. Worked on some of the design for the side bar when it is pulled out.

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// Make nav drop down.
+// slide bar
+const toggle_Btn = document.querySelector(".sidebar-btn");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggle_Btn.addEventListener("click", () => {
+  sidebar.classList.toggle("sidebar-on");
+});
